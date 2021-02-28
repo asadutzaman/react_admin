ccb99b672ec5c7964ae8c8b158e16f81b339ab9f
asadutzaman <mdasadutzaman@gmail.com>
2021-02-28 16:23:47 +0600

    admin lte setup with css broke

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react';
+import Header from './components/Header';
+import SideBar from './components/SideBar';
+import Content from './components/Content';
+
+class App extends Component {
+
+  render() {
+    return (
+      <div>
+        <Header />
+        <SideBar />
+        <Content />
+      </div>
+    );
+  }
 }
 
 export default App;
